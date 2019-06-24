7024d2c0e22c5398f5aad7e989edc460a3635b16
mdelzotto <madelzotto@comcast.net>
2019-06-24 18:47:44 -0400

    web testing challenge completed, mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,36 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  return {
+    ...item,
+  enhancement: item.enhancement < 20 ? item.enhancement + 1 : item.enhancement
+ };
 }
 
 function fail(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability:
+      item.enhancement < 15 ? item.durability - 5 : item.durability - 10,
+    enhancement: item.enhancement > 16 ? item.enhancement - 1 : item.enhancement
+  };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+  if (item.enhancement === 0) {
+    return item;
+  } else {
+    return {
+      ...item,
+      name: `[+${item.enhancement}] ${item.name}`
+    };
+  }
 }
